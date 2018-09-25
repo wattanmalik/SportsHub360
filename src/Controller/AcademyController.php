0ca95dec6476dad04f30b0f81908f84866915e62
wattanmalik <40515805+wattanmalik@users.noreply.github.com>
2018-09-26 00:38:39 +0530

    Academy Controller crud operations

--- src/Controller/AcademyController.php
+++ src/Controller/AcademyController.php
@@ -2,27 +2,89 @@
 
 namespace App\Controller;
 
+use App\Entity\Academy;
+use App\Form\AcademyType;
+use App\Repository\AcademyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/academy")
+ */
 class AcademyController extends AbstractController
 {
-    // public function index()
-    // {
-    //     return $this->json([
-    //         'message' => 'Welcome to your new controller!',
-    //         'path' => 'src/Controller/AcademyController.php',
-    //     ]);
-    // }
+    /**
+     * @Route("/", name="academy_index", methods="GET")
+     */
+    public function index(AcademyRepository $academyRepository): Response
+    {
+        return $this->render('academy/index.html.twig', ['academies' => $academyRepository->findAll()]);
+    }
 
     /**
-     * @Route("/academy/new", name="new_academy")
+     * @Route("/new", name="academy_new", methods="GET|POST")
      */
-    public function new()
+    public function new(Request $request): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller academy!',
-            'path' => 'src/Controller/AcademyController.php',
+        $academy = new Academy();
+        $form = $this->createForm(AcademyType::class, $academy);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($academy);
+            $em->flush();
+
+            return $this->redirectToRoute('academy_index');
+        }
+
+        return $this->render('academy/new.html.twig', [
+            'academy' => $academy,
+            'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="academy_show", methods="GET")
+     */
+    public function show(Academy $academy): Response
+    {
+        return $this->render('academy/show.html.twig', ['academy' => $academy]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="academy_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Academy $academy): Response
+    {
+        $form = $this->createForm(AcademyType::class, $academy);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('academy_edit', ['id' => $academy->getId()]);
+        }
+
+        return $this->render('academy/edit.html.twig', [
+            'academy' => $academy,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="academy_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Academy $academy): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$academy->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($academy);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('academy_index');
     }
 }
